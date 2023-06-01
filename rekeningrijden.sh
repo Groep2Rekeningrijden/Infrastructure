b208632446857a1dd75c2b0cf67aad15067d59d8
David Hellinga <d.hellinga@student.fontys.nl>
2023-06-01 09:26:38 +0200

    Wait for pods to be ready

--- rekeningrijden.sh
+++ rekeningrijden.sh
@@ -10,9 +10,12 @@ deploy(){
   kubectl delete secret -A -l owner=helm,name=$1
 }
 
+wait_for_pods(){
+  kubectl wait --for=condition=Ready pods --all --all-namespaces
+}
 # RUN
 
-sleep 20
+wait_for_pods
 deploy repo
 deploy rabbitmq
 deploy car-service
